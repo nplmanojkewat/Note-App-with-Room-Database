c2061d00b6da58da2683acd8e8a1b04c726b136e
Manoj kewat <nplmrmanoj07@gmail.com>
2025-05-14 23:04:03 +0530

    Fix bug

--- app/src/main/java/com/nplmrmanoj/simplenoteapp/data/remote/NoteDatabase.kt
+++ app/src/main/java/com/nplmrmanoj/simplenoteapp/data/remote/NoteDatabase.kt
@@ -10,30 +10,5 @@ import com.nplmrmanoj.simplenoteapp.data.model.NoteDao
 
 @Database(entities = [Note::class], version = 1)
 abstract class NoteDatabase : RoomDatabase() {
-
     abstract val noteDao: NoteDao
-
-//    companion object {
-//        const val DATABASE_NAME = "Simple Note App"
-//    }
-//
-//    @Volatile
-//    var INSTANCE : NoteDatabase? = null
-//
-//
-//    fun getDatabase(context: Context) : NoteDatabase {
-//        return INSTANCE ?: synchronized(this){
-//
-//            val instance = Room.databaseBuilder(
-//                context,
-//                NoteDatabase::class.java,
-//                DATABASE_NAME
-//            ).build()
-//
-//            INSTANCE = instance
-//            instance
-//
-//        }
-//    }
-
 }
